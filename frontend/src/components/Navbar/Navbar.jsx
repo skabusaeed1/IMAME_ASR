a076f084c6a7bf9de497502cd8494c2680579664
skabusaeed1 <shaikhabusaeed1@gmail.com>
2023-08-06 21:04:44 +0530

    first commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {useState} from 'react'
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+  return (
+    <div>
+        <header>
+        <nav className='flexSB'>
+          <ul className={click ? "mobile-nav" : "flexSB "} onClick={() => setClick(false)}>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/courses'>All Courses</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/team'>Team</Link>
+            </li>
+            <li>
+              <Link to='/pricing'>Pricing</Link>
+            </li>
+            <li>
+              <Link to='/journal'>Journal</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='start'>
+            <div className='nav-heading'>MAKTAB-E-IMAM-E-ASR</div>
+          </div>
+          <button className='toggle' onClick={() => setClick(!click)}>
+            {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
+          </button>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
